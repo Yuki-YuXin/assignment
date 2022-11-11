7411d29d45c7bd15ac886c07c988bd63c310e063
Yu Xin <yuxin@microsoft.com>
2022-11-11 08:30:07 +0000

    10/11/2022

---
+++ app/src/main/java/com/example/assignment/model/MeteorData.kt
@@ -0,0 +1,12 @@
+package com.example.assignment.model
+
+
+open class MeteorData(){
+    var id: String? = null
+    var mass = 0.0
+    var name: String? = null
+    var reclat: String? = null
+    var reclong: String? = null
+    var year: String? = null
+
+}
---
+++ app/src/main/java/com/example/assignment/util/Constant.kt
@@ -0,0 +1,13 @@
+package com.example.assignment.util
+
+class Constant {
+    object Intent {
+        const val METEORITE_NAME = "meteorite_name"
+        const val METEORITE_LAT = "meteorite_lat"
+        const val METEORITE_LNG = "meteorite_long"
+    }
+
+    object Map {
+        const val CAMERA_ZOOM = 4.0f
+    }
+}
